f111471734e0ded9f78da2f5a1654d4b19aed9ef
guthman <oguthman@gmail.com>
2020-11-21 16:21:14 +0200

    Assembler first stage - parsing commands without labels

--- Assembler/StaticDefinitions.c
+++ Assembler/StaticDefinitions.c
@@ -1,69 +1,69 @@
 #define NUMBER_OF_REGISTERS 16
 #define NUMBER_OF_OPCODES 22
-#include <string>
+#include <string.h>
 
 typedef struct
 {
 	char* RegisterName;
-	int RegisterNumber;
+	char* RegisterNumber;
 }Register;
 
 typedef struct
 {
 	char* OpcodeName;
-	int OpcodeNumber
+	char* OpcodeNumber;
 }Opcode;
 
 // Static definitions
 
 Register RegisterMapping[NUMBER_OF_REGISTERS] = {
-	{"$zero", 0},	// Constant zero
-	{"$imm", 1},	// Sign extended immediate
-	{"$v0", 2},		// Result value
-	{"$a0", 3},		// Argument register
-	{"$a1", 4},		// Argument register
-	{"$t0", 5},		// Temporary register
-	{"$t1", 6},		// Temporary register
-	{"$t2", 7},		// Temporary register
-	{"$t3", 8},		// Temporary register
-	{"$s0", 9},		// Saved register
-	{"$s1", 10},	// Saved register
-	{"$s2", 11},	// Saved register
-	{"$gp", 12},	// Global pointer (static data)
-	{"$sp", 13},	// Stack pointer
-	{"$fp", 14},	// Frame Pointer
-	{"$ra", 15},	// Return address
+	{"$zero", "0"},	// Constant zero
+	{"$imm", "1"},	// Sign extended immediate
+	{"$v0", "2"},	// Result value
+	{"$a0", "3"},	// Argument register
+	{"$a1", "4"},	// Argument register
+	{"$t0", "5"},	// Temporary register
+	{"$t1", "6"},	// Temporary register
+	{"$t2", "7"},	// Temporary register
+	{"$t3", "8"},	// Temporary register
+	{"$s0", "9"},	// Saved register
+	{"$s1", "A"},	// Saved register
+	{"$s2", "B"},	// Saved register
+	{"$gp", "C"},	// Global pointer (static data)
+	{"$sp", "D"},	// Stack pointer
+	{"$fp", "E"},	// Frame Pointer
+	{"$ra", "F"},	// Return address
 };
 
 Opcode OpcodeMapping[NUMBER_OF_OPCODES] = {
-	{"add", 0},
-	{"sub", 1},
-	{"and", 2},
-	{"or", 3},
-	{"xor", 4},
-	{"mul", 5},
-	{"sll", 6},
-	{"sra", 7},
-	{"srl", 8},
-	{"beq", 9},
-	{"bne", 10},
-	{"blt", 11},
-	{"bgt", 12},
-	{"ble", 13},
-	{"bge", 14},
-	{"jal", 15},
-	{"lw", 16},
-	{"sw", 17},
-	{"reti", 18},
-	{"in", 19},
-	{"out", 20},
-	{"halt", 21},
+	{"add", "00"},
+	{"sub", "01"},
+	{"and", "02"},
+	{"or", "03"},
+	{"xor", "04"},
+	{"mul", "05"},
+	{"sll", "06"},
+	{"sra", "07"},
+	{"srl", "08"},
+	{"beq", "09"},
+	{"bne", "0A"},
+	{"blt", "0B"},
+	{"bgt", "0C"},
+	{"ble", "0D"},
+	{"bge", "0E"},
+	{"jal", "0F"},
+	{"lw", "10"},
+	{"sw", "11"},
+	{"reti", "12"},
+	{"in", "13"},
+	{"out", "14"},
+	{"halt", "15"},
 };
 
-/*Getting register number by name.
+/*Getting register hex value number by name.
 Return -1 if not found
 */
-int GetRegisterNumber(char* name) {
+char* GetRegisterNumber(char* name) {
 	for (int i = 0; i < NUMBER_OF_REGISTERS; i++)
 	{
 		if (strcmp(RegisterMapping[i].RegisterName, name) == 0)
@@ -73,10 +73,10 @@ int GetRegisterNumber(char* name) {
 	return -1;
 }
 
-/*Getting opcode number by name.
+/*Getting opcode hex value number by name.
 Return -1 if not found
 */
-int GetOpcodeNumber(char* name) {
+char* GetOpcodeNumber(char* name) {
 	for (int i = 0; i < NUMBER_OF_OPCODES; i++)
 	{
 		if (strcmp(OpcodeMapping[i].OpcodeName, name) == 0)
--- Assembler/main.c
+++ Assembler/main.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
-#include <string>
+#define _CRT_SECURE_NO_WARNINGS
 #define MAX_LINE_LENGTH 500
+
+#include <stdio.h>
+#include <string.h>
 
 typedef unsigned int uint;
 typedef struct {
@@ -11,49 +13,118 @@ typedef struct {
 Label* LablesLocations = NULL;
 uint labelsCounter = 0;
 
-void ExtractLabels(FILE* file) {
-	char* line = NULL;
+void RemoveLastChar(char* str) {
+	str[strlen(str) - 1] = '\0';
+}
+
+void GetHexValueOfConstant(char* immValue, char* hexVal) {
+	int base = 16;
+	uint num = atoi(immValue);
+
+	hexVal[5] = '\0';
+	int i = 4;
+
+	do {
+		hexVal[i] = "0123456789ABCDEF"[num % base];
+		i--;
+		num /= base;
+	} while (i >= 0);
+}
+
+int HasImmediate(char* rd, char* rs, char* rt) {
+	if (!strcmp(rd, "$imm,") || !strcmp(rs, "$imm,") || !strcmp(rt, "$imm,"))
+		return 1;
+	if (!strcmp(rd, "$imm") || !strcmp(rs, "$imm") || !strcmp(rt, "$imm"))
+		return 1;
+
+	return 0;
+}
+
+void ExtractLabels(FILE* asmFile) {
+	char line[MAX_LINE_LENGTH];
 	int lineCounter = 0;
 
-	while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
+	while (fgets(line, MAX_LINE_LENGTH, asmFile) != NULL)
 	{
-		char* opcode, rd, rs, rt, immVal;
-		char* label;
+		char opcode[20], rd[20], rs[20], rt[20], immVal[20];
 
 		// Search for command line
-		if (sscanf(line, "%s %s, %s, %s, %s", opcode, rd, rs, rt, immVal) != NULL) {
-			if (!strcmp(rd, "$imm") || !strcmp(rs, "$imm") || !strcmp(rt, "$imm"))
+		if (sscanf(line, "%s %s %s %s %s", opcode, rd, rs, rt, immVal) != NULL) {
+			if (opcode[strlen(opcode) - 1] == ':') { // This is label
+				RemoveLastChar(opcode);
+				Label labelStruct = { opcode, lineCounter };
+				if (LablesLocations == NULL) {
+					LablesLocations = (Label*)calloc(1, sizeof(Label));
+					LablesLocations[0] = labelStruct;
+				}
+				else {
+					LablesLocations = realloc(LablesLocations, (labelsCounter + 1) * sizeof(Label));
+				}
+
+				labelsCounter++;
+			}
+			else if (HasImmediate(rd, rs, rt))
 				lineCounter += 2;
 			else
 				lineCounter++;
-			continue;
 		}
+	}
+}
 
-		// Search for label
-		if (sscanf(line, "%s:", label) != NULL) {
-			Label labelStruct = { label, lineCounter };
-			if (labelsCounter == 0) {
-				LablesLocations = (Label*)calloc(1, sizeof(Label));
-				LablesLocations[0] = labelStruct;
-			}
-			else {
-				LablesLocations = realloc(LablesLocations, (labelsCounter + 1) * sizeof(Label));
-			}
+void ExtractCommands(FILE* asmFile, FILE* imemFile) {
+	char line[MAX_LINE_LENGTH];
+	int lineCounter = 0;
 
-			labelsCounter++;
+	while (fgets(line, MAX_LINE_LENGTH, asmFile) != NULL) {
+		char opcode[20], rd[20], rs[20], rt[20], immVal[20];
+
+		// Search for command line
+		if (sscanf(line, "%s %s %s %s %s", opcode, rd, rs, rt, immVal) != NULL) {
+			if (opcode[strlen(opcode) - 1] == ':') // This is label
+				continue;
+
+			// Removing ','
+			RemoveLastChar(rd); RemoveLastChar(rs); RemoveLastChar(rt);
+
+			// Getting Opcode number.
+			char* opcodeNumber = GetOpcodeNumber(opcode);
+			fputs(opcodeNumber, imemFile);
+
+			// Getting registers numbers
+			char* rdNumber = GetRegisterNumber(rd);
+			fputs(rdNumber, imemFile);
+			char* rsNumber = GetRegisterNumber(rs);
+			fputs(rsNumber, imemFile);
+			char* rtNumber = GetRegisterNumber(rt);
+			fputs(rtNumber, imemFile);
+			fputc('\n', imemFile);
+
+			// if there is $imm then convert immVal to Hex
+			if (HasImmediate(rd, rs, rt)) {
+				char hexValue[6];
+				GetHexValueOfConstant(immVal, &hexValue);
+				fputs(hexValue, imemFile);
+				fputc('\n', imemFile);
+			}
 		}
 	}
 }
 
 
 int main(int argc, char* argv[]) {
-	if (argc != 3)
+	if (argc != 4)
 		return 1;
 	
-	FILE* asmFile;
-	if ((asmFile = fopen(argv[0], "r")) == NULL) {
+	FILE* asmFile = fopen(argv[1], "r");
+	FILE* imemFile = fopen(argv[2], "w");
+	if (asmFile == NULL || imemFile == NULL) {
 		printf("Error! opening file");
 		exit(1);
 	}
 
+	//ExtractLabels(asmFile);
+	ExtractCommands(asmFile, imemFile);
+	
+	fclose(asmFile);
+	fclose(imemFile);
 }
